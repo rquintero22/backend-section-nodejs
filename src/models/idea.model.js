6d5b55a588e6f50c9561a74e43cb52c98f72614e
Raymond Quintero Aparicio <quinteroaparicio@gmail.com>
2021-12-01 20:33:13 -0600

    [ADD]monggose autopopulate added

---
+++ src/models/idea.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const IdeaSchema = new Schema({
+    idea: { type: String, required: true },
+    description: { type: String },
+    upvotes: [{ type: Boolean }],
+    downvotes: [{ type: Boolean }],
+    author: { type: Schema.Types.ObjectId, ref: 'user', required: true, autopapulate: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'comment', required: true, autopapulate: true }, ]
+});
+
+IdeaSchema.plugin(require('mongoose-autopopulate'));
+
+module.exports = mongoose.model('idea', IdeaSchema);
