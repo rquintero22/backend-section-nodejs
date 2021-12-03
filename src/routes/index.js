732906553f68fe9b0aaf5fde2f447ba31c475bad
Raymond Quintero Aparicio <quinteroaparicio@gmail.com>
2021-12-02 19:50:49 -0600

    [ADD] routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ require('express-async-errors');
 
 const { NotFoundMiddleware, ErrorMiddleware } = require('../middlewares');
 
-module.exports = function({ HomeRoutes }) {
+module.exports = function({ HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes }) {
     const router = express.Router();
     const apiRoutes = express.Router();
 
@@ -17,6 +17,9 @@ module.exports = function({ HomeRoutes }) {
         .use(compression());
 
     apiRoutes.use('/home', HomeRoutes);
+    apiRoutes.use('/user', UserRoutes);
+    apiRoutes.use('/idea', IdeaRoutes);
+    apiRoutes.use('/comment', CommentRoutes);
 
     router.use('/v1/api', apiRoutes);
 
--- src/startup/container.js
+++ src/startup/container.js
@@ -11,7 +11,7 @@ const { HomeService, UserService, IdeaService, CommentService } = require('../se
 const { HomeController, UserController, IdeaController, CommentController } = require('../controllers');
 
 // Routes
-const { HomeRoutes } = require('../routes/index.routes');
+const { HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes } = require('../routes/index.routes');
 const Routes = require('../routes');
 
 // Models
@@ -42,7 +42,10 @@ container
         CommentController: asClass(CommentController.bind(CommentController)).singleton(),
     })
     .register({
-        HomeRoutes: asFunction(HomeRoutes).singleton()
+        HomeRoutes: asFunction(HomeRoutes).singleton(),
+        UserRoutes: asFunction(UserRoutes).singleton(),
+        IdeaRoutes: asFunction(IdeaRoutes).singleton(),
+        CommentRoutes: asFunction(CommentRoutes).singleton(),
     })
     .register({
         User: asValue(User),
